d2e909015a7115648a93a79cff01aa45f3971e21
seacrest3 <o7716442@gmail.com>
2023-02-11 22:04:09 +0000

    linked both weather and REST counties APIs to servers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,50 @@
 let countryPage = false;
 
-let countriesVisited = []
+let countriesVisited = [];
+let userName = "";
+let countryName = "";
+let isNewUser = "";
 
-function callSavedData(){
-    $(".countries-visited-container").empty()
-    let storedData = localStorage.getItem("countries")
-    if (storedData === null){
-        countriesVisited = []
-    } else if (typeof storedData === "string"){
-        countriesVisited.push(storedData)
-        displayHistory(countriesVisited)
-    } else {
-        storedData.forEach(function(element){
-            countriesVisited.push(element)
-        })
-        displayHistory(countriesVisited)
-    } 
-}
-function displayHistory (array){
-    $(".countries-visited-container").empty()
-    if (array.length === 1){
-        let newDiv = $(`<div>${array[0]}</div>`)
-        $(".countries-visited-container").append(newDiv)       
-    }
-    else {
-        array.forEach(function(element){
-            let newDiv = $(`<div>${element}</div>`)
-            $(".countries-visited-container").append(newDiv)
-        })
-    }
-}
+// function callSavedData(){
+//     $(".countries-visited-container").empty()
+//     let storedData = localStorage.getItem("countries")
+//     if (storedData === null){
+//         countriesVisited = []
+//     } else if (typeof storedData === "string"){
+//         countriesVisited.push(storedData)
+//         displayHistory(countriesVisited)
+//     } else {
+//         storedData.forEach(function(element){
+//             countriesVisited.push(element)
+//         })
+//         displayHistory(countriesVisited)
+//     } 
+// }
+// function displayHistory (array){
+//     $(".countries-visited-container").empty()
+//     if (array.length === 1){
+//         let newDiv = $(`<div>${array[0]}</div>`)
+//         $(".countries-visited-container").append(newDiv)       
+//     }
+//     else {
+//         array.forEach(function(element){
+//             let newDiv = $(`<div>${element}</div>`)
+//             $(".countries-visited-container").append(newDiv)
+//         })
+//     }
+// }
+
 // logs weather response based on city parameter
 function getWeatherCondition(city) {
-    
+
+    console.log("The value of city: " + city);
+    // API Key for Open Weather API
     const weatherApiKey = "3102f3b643256623c7321b2ed4853779"
     // Constructing a URL to search for current weather data
-    const queryURLCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`
-    // API Key for Open Weather API
+    const queryURLCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`;
+
 
     // Performing AJAX GET request
-                            // $("#today").empty(); -- What is this doing?
     $.ajax({
         url: queryURLCurrent,
         method: "GET"
@@ -54,71 +59,171 @@ function getWeatherCondition(city) {
 }
 
 
-
-
-$(function(){
+$(function () {
     // retrieves saved values and displays them topleft
-    callSavedData()
+    // callSavedData()
     // Home Page
-    if (!countryPage){
-    let welcomeMessage = $(`<p class="speech-bubble-text">Hi User, I'm Globey, It's nice to meet you</p>`);
-    $(".speech-bubble-container").append(welcomeMessage);
-    $("#flag-container").empty()
-    $(".btns-container").addClass("hide")
-    $("#radio-div").addClass("hide")
+    if (!countryPage) {
+        let welcomeMessage = $(`<p class="speech-bubble-text">Hi User, I'm Globey. What is your name?</p>`);
+        $(".speech-bubble-container").append(welcomeMessage);
+        $("#flag-container").empty()
+        $(".btns-container").addClass("hide")
+        $("#radio-div").addClass("hide")
     }
-
-
-    // Search
-    $("#search-form").on("submit", function(event){
+    // Input form for user name
+    $("#name-input-form").on("submit", function (event) {
         event.preventDefault();
         countryPage = true;
-        $(".countries-visited-container").addClass("hide")
-        clear();
-        let countryName = $("#search-input").val().trim()
-        $("#search-input").val("")
-        console.log(countryName);
-        const countriesURL = `https://restcountries.com/v2/name/${countryName}`
+        userName = $("#name-input").val().trim();
+        console.log("The value of userName: " + userName)
+        $("#name-input").val("");
+        $("#name-input-form").hide();
+        $("#country-input-form").removeClass("hide");
+        $(".speech-bubble-text").hide();
+        welcomeMessage = $(`<p class="speech-bubble-text">Hi! Where are you going today</p>`);
+        $(".speech-bubble-container").append(welcomeMessage);
+    });
 
+    $("#country-input-form").on("submit", function (event) {
+        event.preventDefault();
+        countryName = $("#country-input").val().trim();
+        console.log("The value of countryName: " + countryName)
+        $("#country-input-form").val("");
+        $("#country-input-form").hide();
+        $("#welcome").removeClass("hide");
+        $(".speech-bubble-text").hide();
+        $("#globey-container").removeClass("hide");
+        console.log("The value of countryName: " + countryName)
+        var queryURLCountry = "https://restcountries.com/v2/name/" + countryName;
         $.ajax({
-            url: countriesURL,
-            method: "GET",
-            error: function (err) {
-                displayErrorScreen();
-            }
+            url: queryURLCountry,
+            method: "GET"
+            // ,
+            // error: function (err) {
+            //     displayErrorScreen();
+            // }
         }).then(function (response) {
-            $("#globey").attr("src", "assets/images/Global Image.svg")
-            welcomeMessage = (`<p class="speech-bubble-text">Welcome to ${response[0].name}`)
-            $(".btns-container").removeClass("hide")
-            $(".speech-bubble-container").append(welcomeMessage)
-            $('#radio-div').removeClass("hide")
-            $('#saveBtn').removeClass("hide")
-            $('#saveBtn').text(`I've been to ${response[0].name} `)
+            console.log("The value of response[0].name: " + response[0].name);
+            welcomeMessage = (`<p class="speech-bubble-text">Welcome to ${response[0].name}`);
+            $(".speech-bubble-container").append(welcomeMessage);
+            $("#flag-container").empty();
+            let flag = $(`<img src="${response[0].flag}" class="flag">`);
+            $("#flag-container").append(flag);
             
-            
-            // Add Flag
+            // $(".btns-container").removeClass("hide");
+            // $(".speech-bubble-container").append(welcomeMessage);
+            // $("#radio-div").removeClass("hide");
+            // $("#saveBtn").removeClass("hide");
+            // $("#saveBtn").text("I've been to ${response[0].name}");
+
+        });
+    });
+
+    $("#welcome-globey").on("click", function (event) {
+        event.preventDefault();
+        // $(".speech-bubble-text").hide();
+        console.log("The value of countryName: " + countryName)
+
+        var queryURLCountry = "https://restcountries.com/v2/name/" + countryName;
+        $.ajax({
+            url: queryURLCountry,
+            method: "GET"
+            // ,
+            // error: function (err) {
+            //     displayErrorScreen();
+            // }
+        }).then(function (response) {
+            console.log("The value of 1st ${response[0].name}: " + response[0].name);
+            // welcomeMessage = (`<p class="speech-bubble-text">Welcome to ${response[0].name}`)
+            // $(".speech-bubble-container").append(welcomeMessage);
             $("#flag-container").empty();
             let flag = $(`<img src="${response[0].flag}" class="flag">`)
-            $("#flag-container").append(flag)
+            $("#flag-container").append(flag);
+        });
+    });
+    $("#welcome-globey").on("click", function (event) {
+        event.preventDefault();
+        $(".speech-bubble-text").hide();
+
+        var queryURLCountry = "https://restcountries.com/v2/name/" + countryName;
+        $.ajax({
+            url: queryURLCountry,
+            method: "GET"
+            // ,
+            // error: function (err) {
+            //     displayErrorScreen();
+            // }
+        }).then(function (response) {
+            console.log("The value of 2nd ${response[0].name}: " + response[0].name);
+            welcomeMessage = (`<p class="speech-bubble-text">What would you like to know about ${response[0].name} ?`);
+            // $("#flag-container").hide();
+            $(".speech-bubble-container").append(welcomeMessage);
+            $(".btns-container").removeClass("hide");
+            $('#radio-div').removeClass("hide");
+            $('#saveBtn').removeClass("hide");
+            $('#saveBtn').text(`I've been to ${response[0].name} `);
+        })
+    });
+    // Search
+    // $("#search-form").on("submit", function (event) {
+    //     event.preventDefault();
+        // countryPage = true;
+        // $(".countries-visited-container").addClass("hide")
+        // clear();
+        // let countryName = $("#search-input").val().trim()
+        // $("#search-input").val("")
+        // console.log(countryName);
+        // const countriesURL = `https://restcountries.com/v2/name/${countryName}`
+
+    //     $.ajax({
+    //         url: countriesURL,
+    //         method: "GET",
+    //         error: function (err) {
+    //             displayErrorScreen();
+    //         }
+    //     }).then(function (response) {
+    //         $("#globey").attr("src", "assets/images/Global Image.svg")
+    //         welcomeMessage = (`<p class="speech-bubble-text">Welcome to ${response[0].name}`)
+    //         $(".btns-container").removeClass("hide")
+    //         $(".speech-bubble-container").append(welcomeMessage)
+    //         $('#radio-div').removeClass("hide")
+    //         $('#saveBtn').removeClass("hide")
+    //         $('#saveBtn').text(`I've been to ${response[0].name} `)
+
+
+    //         // Add Flag
+    //         $("#flag-container").empty();
+    //         let flag = $(`<img src="${response[0].flag}" class="flag">`)
+    //         $("#flag-container").append(flag)
 
             // Add functionality to buttons
-            $(".btns-container").on("click", ".btn", function(event){
+            $(".btns-container").on("click", ".btn", function (event) {
                 event.preventDefault()
                 let buttonClicked = event.target.innerHTML;
-                showInfo(response, buttonClicked)
-            } )
+                var queryURLCountry = "https://restcountries.com/v2/name/" + countryName;
+                $.ajax({
+                    url: queryURLCountry,
+                    method: "GET"
+                    // ,
+                    // error: function (err) {
+                    //     displayErrorScreen();
+                    // }
+                }).then(function (response) {
+                showInfo(response, buttonClicked);
+                });
+            })
 
             // saves country name
-            $('#saveBtn').click(function(event){
+            $('#saveBtn').click(function (event) {
                 event.preventDefault()
                 let countryToSave = response[0].name;
-                countriesVisited.push(countryToSave)
-                localStorage.setItem("countries", countriesVisited)  
-                callSavedData()     
+                countriesVisited.push(countryToSave);
+                localStorage.setItem("countries", countriesVisited);
+                callSavedData();
             })
-            
-    })
-    })
+
+        // })
+    // })
 
     // Home Button
     $("#home-button").click(function(event){
@@ -136,14 +241,43 @@ $(function(){
     })
 })
 
-// Function to empty out the speechbubble
+// $(document).ready(function () {
+//     $("#globey-1").on("click", function (event) {
+//         // Prevent the default behavior
+//         event.preventDefault();
+
+//         // create user object from submission
+//         let userInput = [{
+//             name: $("#name-input").val(),
+//             country: $("#country-input").val(),
+//             newUser: $("#check").is(":checked"),
+//         }];
+
+//         console.log("The value of userInput: " + JSON.stringify(userInput));
+//         console.log("The value of user.name: " + userInput[0].name);
+//         console.log("The value of user.country: " + userInput[0].country);
+//         console.log("The value of user.visit: " + userInput[0].newUser);
+
+//         var userInf = JSON.parse(localStorage.getItem("userLog"));
+//         console.log("The value getting from localStorage: " + JSON.stringify(userInf));
+        
+//         userInf.push(userInput[0]);
+//         localStorage.setItem("userLog", JSON.stringify(userInf));
+//         var userInf2 = JSON.parse(localStorage.getItem("userLog"));
+//         console.log("The value getting from localStorage after pushing: " + JSON.stringify(userInf2));
+//         console.log("No. of Users: " + userInf2.length);
+//     });
+// })
+
+//Function to empty out the speechbubble
 function clear() {
     $(".speech-bubble-container").empty();
   }
 
-// // Function to display the country info in the bubble
+// Function to display the country info in the bubble
 function showInfo(response, buttonClicked) {
     clear();
+    console.log("The value of response: " + response);
     let info;
     switch (buttonClicked){
         case 'Population':
@@ -172,12 +306,11 @@ function showInfo(response, buttonClicked) {
     }   
 }
 
-function displayErrorScreen(){
-    let errorMessage = $(`<p class="speech-bubble-text">Oh Oh! I don't think that's a country </p>`);
-    $(".speech-bubble-container").append(errorMessage);
-    $("#globey").attr("src", "assets/images/sad-globey.svg")
-    $(".flag-container").empty()
-    $(".btns-container").addClass("hide")
-    $("#saveBtn").addClass("hide")
-
-}
+// function displayErrorScreen(){
+//     let errorMessage = $(`<p class="speech-bubble-text">Oh Oh! I don't think that's a country </p>`);
+//     $(".speech-bubble-container").append(errorMessage);
+//     $("#globey").attr("src", "assets/images/sad-globey.svg")
+//     $(".flag-container").empty()
+//     $(".btns-container").addClass("hide")
+//     $("#saveBtn").addClass("hide")
+// }
